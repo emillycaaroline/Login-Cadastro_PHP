707950a72604854f5ea502126a556c34e791dc1e
emillycaaroline <emillyjchagas@gmail.com>
2024-05-28 18:41:08 -0300

    Adicionei as informações na página inicial e meu gitHub.

--- Inicio.php
+++ Inicio.php
@@ -231,8 +231,8 @@
         </header>
 
         <main class="px-3">
-            <h1>Cover your page.</h1>
-            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            <h1>Pagina de Cadastro e Login.</h1>
+            <p class="lead">Essa página feita em PHP foi realizada para uma atividade de Programação web II com a ultilização de bootstrap para modelo. <br>A baixo o segue o modelo e meu GitHub.</p>
             <p class="lead">
                 <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
             </p>
@@ -240,6 +240,8 @@
 
         <footer class="mt-auto text-white-50">
             <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+
+                <p><a href="https://github.com/emillycaaroline" class="text-white">GitHub</a>
         </footer>
     </div>
     <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
