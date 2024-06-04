5ff054f51a58e0aff3bb05c867b51407a92df5e4
emillycaaroline <emillyjchagas@gmail.com>
2024-06-04 17:53:19 -0300

    Deixa tudo presente em nav no centro da página.

--- cadastro.php
+++ cadastro.php
@@ -183,8 +183,7 @@
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
         <header class="mb-auto">
             <div>
-            <h3 class="float-md-start mb-0">Cover</h3>
-                <nav class="nav nav-masthead justify-content-center float-md-end">
+            <nav class="nav nav-masthead d-flex  justify-content-center ">
                     <a class="nav-link fw-bold py-1 px-0" aria-current="page" href="Inicio.php">Inicio</a>
                     <a class="nav-link fw-bold py-1 px-0" href="login.php">Login</a>
                     <a class="nav-link fw-bold py-1 px-0 active" href="cadastro.php">Cadastro</a>
